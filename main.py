6a8fd4c0a52a9b2527ac4cb41581ef10e2689a55
Bill Dahm <onkirkwoodhill@sbcglobal.net>
2017-02-15 16:37:11 -0600

    Completed minimal assignment requirements before cleaning up code and html appearances.

--- main.py
+++ main.py
@@ -32,7 +32,16 @@ class Blog(db.Model):
     blog=db.TextProperty(required=True)
     created=db.DateTimeProperty(auto_now_add=True)
 
-class MainHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+    """ Base RequestHandler class for the app.
+        The other handlers inherit from this one.
+    """
+    def renderError(self,error_code,msg):
+        """ Sends HTTP error code and error detail supplied by the app """
+        self.error(error_code)
+        self.response.write(msg)
+
+class RootHandler(BaseHandler):
 #class MainHandler(Handler):
     """ Handles requests coming in to '/'  """
 
@@ -69,7 +78,7 @@ class MainHandler(webapp2.RequestHandler):
             content = t.render(title=title,blog=blog,error=incomplete_err)
             self.response.write(content)
 
-class ListBlogs(MainHandler):
+class ListBlogs(BaseHandler):
     """ Handles requests coming in to /list """
 
     def get(self):
@@ -82,9 +91,18 @@ class ListBlogs(MainHandler):
         self.response.write(content)
         #self.response.write("Control passed to /list")
     def post(self):
-        self.response.write("Control passed to /list")
+        # Add the blog.key().id() here ...
+        #self.response.write("Setting up the 'a' object ...")
+        #a=db.GqlQuery("SELECT * FROM Blog WHERE title=title")
+        k=blog.key(a).id()
+        if not k:
+            self.response.write("Object k was not established")
+        else:
+            self.response.write("Object k was established")
+            #self.redirect("/blog/"+str(int(k)))
+            #self.response.write("Control passed to /list")
 
-class AddBlogs(MainHandler):
+class AddBlogs(BaseHandler):
     def get(self):
         t = jinja_env.get_template("post-new.html")
 
@@ -103,8 +121,13 @@ class AddBlogs(MainHandler):
 
             a=Blog(title=title,blog=blog) # Create an instance of Blog ...
             a.put()
-
-            self.redirect("/")
+            # Add the blog.key().id() here ...
+            k=Blog.key(a).id()
+            if not k:
+                self.response.write("Object k was not established")
+            else:
+                #self.response.write("Object k was established")
+                self.redirect("/blog/"+str(int(k)))
         else:
             incomplete_err = "we need both a title and some blog contents!"
 
@@ -116,14 +139,22 @@ class AddBlogs(MainHandler):
 
         #self.response.write("Control passed to /add")
 
-class ViewPostHandler(webapp2.RequestHandler):
+#class ViewPostHandler(webapp2.RequestHandler):
+class ViewBlog(BaseHandler):
     def get(self, id):
         #pass #replace this with some code to handle the request
-        self.response.write(id)
+        #instance=Blog.get_by_id(int('5770237022568448'),Parent=None)
+        rec=Blog.get_by_id(int(id))
+        if rec:
+            t = jinja_env.get_template('blog-detail.html')
+            content=t.render(blog=rec)
+            self.response.write(content)
+        else:
+            self.renderError(404,"Record Not Found !")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/', RootHandler),
     ('/list', ListBlogs),
     ('/add', AddBlogs),
-    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+    webapp2.Route('/blog/<id:\d+>', ViewBlog)
     ], debug=True)
